b18194c72fff15ea9d9fb95fdab72ae5bcf3416f
Craig Darmetko <cdarmetk@gmail.com>
2013-07-26 14:11:09 +0000

    trying to add user login

---
+++ config/dbschema.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose'),
+	bcrypt = require('bcrypt'),
+	SALT_WORK_FACTOR = 10;
+exports.mongoose = mongoose;
+
+// Database connect
+var uristring = 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/test';
+
+var mongoOptions = { db: { safe: true }};
+
+mongoose.connect(uristring, mongoOptions, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Successfully connected to: ' + uristring);
+  }
+});
+
+//******* Database schema TODO add more validation
+var Schema = mongoose.Schema, 
+	ObjectId = Schema.ObjectId;
+
+// User schema
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  //accounttype: { type: String, required: true},
+  //card: { type: String, required: false},
+  //subscriptionvalid { type: String, required: false},
+  admin: { type: Boolean, required: true },
+  //tasteid: { type: String, required:true, unique:true},
+});
+
+
+// Bcrypt middleware
+userSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) return next();
+
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if(err) return next(err);
+
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if(err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+// Password verification
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if(err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+// Export user model
+var userModel = mongoose.model('User', userSchema);
+exports.userModel = userModel;
--- web.js
+++ web.js
@@ -1,13 +1,39 @@
-var express = require('express');
-var fs = require("fs");
+var express = require('express')
+  , app = express()
+  , db = require('./config/dbschema')
+  , pass = require('./config/pass')
+  , passport = require('passport')
+  , basic_routes = require('./routes/basic')
+  , user_routes = require('./routes/user');
+/*var fs = require("fs");
 
-var app = express.createServer(express.logger());
-app.use(express.static(__dirname));
-var index = fs.readFileSync("index.html");
-var home = fs.readFileSync("home.html");
-var about = fs.readFileSync("about.html");
+//var app = express.createServer(express.logger());
+var index = fs.readFileSync("views/index2.html");
+var home = fs.readFileSync("views/home.html");
+var about = fs.readFileSync("views/about.html");*/
 
-app.get('/', function(request, response) {
+
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger('dev'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'supersecretsecrets' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  //app.use(express.static(__dirname + '/../../public'));
+ app.use(express.static(__dirname));
+ 
+});
+
+/*app.get('/index2.html', function(request, response) {
   response.send(index.toString());
 });
 
@@ -17,7 +43,16 @@ app.get("/about.html", function(request, response){
 
 app.get("/home.html", function(request, response){
 	response.send(home.toString());
-});
+});*/
+
+app.get('/', basic_routes.index);
+
+// User pages
+app.get('/account', pass.ensureAuthenticated, user_routes.account);
+app.get('/login', user_routes.getlogin);
+app.post('/login', user_routes.postlogin);
+app.get('/admin', pass.ensureAuthenticated, pass.ensureAdmin(), user_routes.admin);
+app.get('/logout', user_routes.logout);
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
